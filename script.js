0c1b448d8d34235ef6be0dd061a8a710a65c2f9b
Sadia Naaz <139625425+Sadia-Naaz@users.noreply.github.com>
2023-11-19 11:56:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ document.addEventListener("DOMContentLoaded",function(){
 var counter = document.getElementById("counter");
 var btn = document.getElementById("incrementBtn");
 var count = 0;
-function incrementCount() {
+btn.addEventListener('click',function(){
 alert(count);
 count++;
 counter.innerText = count;
-}
+});
 btn.addEventListener('click',incrementCount);
 });
